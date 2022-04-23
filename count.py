fd23804a95dc97d6de419af43f0e981888117b5f
DevaDarshni <85098562+DevaDarshni@users.noreply.github.com>
2022-04-23 17:57:01 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,12 @@
+string=input("enter string")
+charatercount=0
+wordcount=1
+for i in string:
+    charatercount=charatercount+1
+    if(i ==' '):
+        wordcount=wordcount+1
+
+print("no.of word in string")
+print(word count)
+print("no.charater in string")
+print(charater count)
